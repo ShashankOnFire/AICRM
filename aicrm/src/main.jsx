c799988abae3bf43c604dd6b983d6a86d6f310a8
Manas Bhasin <manas_bhasin@yahoo.com>
2024-03-12 00:38:48 +0530

    sign in option

--- aicrm/src/main.jsx
+++ aicrm/src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { ClerkProvider } from '@clerk/clerk-react';
+import { toByteArray } from 'base64-js';
+
+const publishableKey = 'pk_test_dXAtZHJha2UtMzAuY2xlcmsuYWNjb3VudHMuZGV2JA';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ClerkProvider publishableKey={publishableKey}>
+      <App />
+    </ClerkProvider>
+  </React.StrictMode>
+);
